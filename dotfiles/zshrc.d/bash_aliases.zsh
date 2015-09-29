62cb479b3316ee71aea4e40bb78c2184cb70b86d
Sergio Ruiz <serginator@gmail.com>
2015-09-29 10:01:32 +0200

    Improved update alias

--- dotfiles/zshrc.d/bash_aliases.zsh
+++ dotfiles/zshrc.d/bash_aliases.zsh
@@ -1,5 +1,4 @@
 alias please='sudo'
-alias update='brew update && brew upgrade --all && brew cask update'
 alias install='brew install'
 alias tarsvn='tar --exclude={.svn,.idea} -z -c -v -f '
 alias targit='tar --exclude={.git,.idea} -z -c -v -f '
@@ -19,10 +18,22 @@ alias screenMac='screen /dev/cu.SLAB_USBtoUART 115200'
 alias dockerIp='boot2docker ip 2>/dev/null'
 alias g='git'
 alias gr='grunt'
+alias vncon='sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -configure -allowAccessFor -allUsers -configure -restart -agent -privs -all'
+alias vncoff='sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off'
+
+update() {
+  echo -e "*** brew/cask ***"
+  brew update && brew upgrade --all && brew cask update
+  sleep 1
+  echo -e "*** Node/npm ***"
+  sudo n stable && curl -L https://www.npmjs.com/install.sh | sudo sh && sudo npm update -g
+  sleep 1
+  echo -e "*** apm ***"
+  apm update --confirm=false
+  sleep 1
+}
 
 killPattern() {
   ps aux | grep $1 | awk '{print $2}' | xargs kill -9
 }
 
-alias vncon='sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -configure -allowAccessFor -allUsers -configure -restart -agent -privs -all'
-alias vncoff='sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off'
